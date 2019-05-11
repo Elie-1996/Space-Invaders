0f218b827293cbee0380e728dc7b007b00cb0c8a
Elie-1996 <44254485+Elie-1996@users.noreply.github.com>
2019-05-11 21:10:04 +0300

    Update Movement.cs
    Fixed weird Bug + Added speed scaling for rotation

--- Space Invaders/Assets/Scripts/Movement.cs
+++ Space Invaders/Assets/Scripts/Movement.cs
@@ -22,18 +22,18 @@ public class Movement : MonoBehaviour
             Debug.LogError(gameObject.name + " (Movement.cs): No Rigidbody component was found!");
             return;
         }
+        float timePassed = Time.deltaTime;
 
-        HandleMovement(rigidbody);
-        HandleRotation(rigidbody);
+        HandleMovement(rigidbody, timePassed);
+        HandleRotation(rigidbody, timePassed);
     }
 
-    private void HandleMovement(Rigidbody rigidbody)
+    private void HandleMovement(Rigidbody rigidbody, float time)
     {
         float realSpeed = moveSpeed * Utils.speedScale;
         float verticalDirection = Input.GetAxis("Vertical");
         float horizontalDirection = Input.GetAxis("Horizontal");
-        float timePassed = Time.deltaTime;
-        float distance = timePassed * realSpeed;
+        float distance = time * realSpeed;
 
         Vector3 moveAmount = distance * (verticalDirection * rigidbody.transform.forward + horizontalDirection * rigidbody.transform.right);
 
@@ -41,14 +41,15 @@ public class Movement : MonoBehaviour
 
     }
 
-    private void HandleRotation(Rigidbody rigidbody)
+    private void HandleRotation(Rigidbody rigidbody, float time)
     {
-        float upRotationInput = Input.GetAxis("Mouse X");
-        float roundRotationInput = Input.GetAxis("Mouse Y");
+        float upRotationInput = Input.GetAxis("Mouse Y");
+        float roundRotationInput = Input.GetAxis("Mouse X");
 
-        Vector3 rightRotation = rigidbody.transform.right;
-        Vector3 upRotation = rigidbody.transform.up;
+        Vector3 upRotation = rigidbody.transform.right;
+        Vector3 rightRotation = rigidbody.transform.up;
         Vector3 rotateAmount = roundRotationInput * roundRotationSpeed * rightRotation + upRotationInput * upRotationSpeed * upRotation;
+        rotateAmount *= time * Utils.speedScale;
         rigidbody.transform.Rotate(rotateAmount);
     }
 }
