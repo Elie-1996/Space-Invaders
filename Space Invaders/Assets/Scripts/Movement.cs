766b7f9264e54f5f8374fd6823f5f6d2657b5651
Basel <z.basel324@gmail.com>
2019-06-03 21:14:03 +0300

    impleminting shoots

--- Space Invaders/Assets/Scripts/Movement.cs
+++ Space Invaders/Assets/Scripts/Movement.cs
@@ -7,12 +7,31 @@ public class Movement : MonoBehaviour
     public float moveSpeed;
     public float rotationSpeed;
 
+    public GameObject rocket1;
+    public Transform rocket1Shot;
+    public GameObject rocket2;
+    public Transform rocket2Shot;
+    public float fireRate;
+    private float nextFire;
     private void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
+    private void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(rocket1, rocket1Shot.position, rocket1Shot.rotation);
+        }
+        if (Input.GetButton("Fire2") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(rocket2, rocket2Shot.position, rocket2Shot.rotation);
+        }
+    }
     private void FixedUpdate()
     {
         Rigidbody rigidbody = GetComponent<Rigidbody>();
