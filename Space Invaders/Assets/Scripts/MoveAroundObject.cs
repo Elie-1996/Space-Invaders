fae8d30f35af81f977ec30e9ce062845d371baee
Basel <z.basel324@gmail.com>
2019-06-18 15:15:38 +0300

    impleminting enemy movment

--- Space Invaders/Assets/Scripts/MoveAroundObject.cs
+++ Space Invaders/Assets/Scripts/MoveAroundObject.cs
@@ -4,11 +4,51 @@ using UnityEngine;
 
 public class MoveAroundObject : MonoBehaviour
 {
+    private Transform target;
+    Vector3 storeTarget;
+    bool savePos;
 
+    Vector3 acceleration;
+    Vector3 velocity;
+    public float maxSpeed = 5;
+    float storeMaxSpeed;
+    float targetSpeed;
+
+    Rigidbody rigidbody;
+    public List<Vector3> EscapeDirections = new List<Vector3>();
     // Start is called before the first frame update
     void Start()
     {
+        storeMaxSpeed = maxSpeed;
+        targetSpeed = storeMaxSpeed;
 
+        rigidbody = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag(Utils.TagPlayer).transform;
     }
 
+    private void FixedUpdate()
+    {
+        target = GameObject.FindGameObjectWithTag(Utils.TagPlayer).transform;
+        Debug.DrawLine(transform.localPosition, target.position);
+        Vector3 forces = MoveForwardTarget(target.position);
+        acceleration = forces;
+
+        velocity += 2 * acceleration * Time.deltaTime;
+
+        //if(velocity.magnitude  > maxSpeed)
+        //{
+        //    velocity = velocity.normalized * maxSpeed;
+        //}
+
+        rigidbody.velocity = velocity;
+        Quaternion quaternion = Quaternion.LookRotation(velocity);
+        transform.rotation = Quaternion.Slerp(transform.rotation, quaternion, Time.deltaTime * 3);
+    }
+
+    Vector3 MoveForwardTarget(Vector3 target)
+    {
+        Vector3 distance = target - transform.position;
+
+            return distance.normalized * maxSpeed;
+    }
 }
