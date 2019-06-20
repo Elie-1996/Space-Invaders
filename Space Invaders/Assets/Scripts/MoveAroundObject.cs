73e7f700f63fe71f06b21b83de1aba1c99eff42e
Elie-1996 <44254485+Elie-1996@users.noreply.github.com>
2019-06-20 10:24:20 +0300

    Finishing Enemy Attacks + Movement
    * Enemy can attack in two different ways now.
    * Enemy also has random movement.
    * Enemy can hurt the player now.

--- Space Invaders/Assets/Scripts/MoveAroundObject.cs
+++ Space Invaders/Assets/Scripts/MoveAroundObject.cs
@@ -7,21 +7,51 @@ using UnityEngine;
 public class MoveAroundObject : MonoBehaviour
 {
     public float speed;
+    public float closeApproximity;
 
+    public GameObject radiusCenter;
+    public float radiusMovement;
+    private float timeCounter;
+
+    private int attackType;
     private Rigidbody enemyRigidBody;
     private GameObject[] players;
     private int playerIndexToAttack;
 
+    private Vector3 randomTarget;
+    private float gameRadius;
+    private bool decidedRandomPosition;
+    private float waitTimeRandomMovementPassed;
+
     private void Start()
     {
+        waitTimeRandomMovementPassed = 0.0f;
+        decidedRandomPosition = false;
+        gameRadius = Utils.getGameBoundaryRadius(GameObject.FindGameObjectWithTag(Utils.TagBackground));
+        timeCounter = 0.0f;
         enemyRigidBody = GetComponent<Rigidbody>();
         if (enemyRigidBody == null) throw new MissingComponentException("Enemy: Rigidbody is missing.");
 
         playerIndexToAttack = -1;
         ChoosePlayerToAttack();
         if (players.Length <= 0) throw new System.Exception("Not enough players. Should not load Enemy");
+
+
+        attackType = -1;
+        DecideAttackType();
     }
 
+    private Transform FindTransformInChildWithTag(string _tag)
+    {
+        foreach (Transform child in gameObject.transform)
+        {
+            if( child.tag == _tag)
+            {
+                return child;
+            }
+        }
+        return null;
+    }
 
     // physics
     private void FixedUpdate()
@@ -29,13 +59,75 @@ public class MoveAroundObject : MonoBehaviour
         if (playerIndexToAttack == -1) return;
         GameObject player = players[playerIndexToAttack];
         FaceTarget(player.transform);
-        MoveTowardsTarget(player.transform);
+        Attack(player.transform);
+    }
+
+    private void Attack(Transform targetTransform)
+    {
+        if (attackType == 1)
+            MoveTowardsTarget(targetTransform);
+        else if (attackType == 2)
+            CirculateAroundTarget(targetTransform);
+        else if (attackType == 3)
+            RandomMovement();
+        else
+            throw new System.Exception("Incorrect attack type was = " + attackType);
+    }
+
+    private void RandomMovement()
+    {
+        waitTimeRandomMovementPassed += Time.deltaTime;
+        if (decidedRandomPosition == false || randomTarget == transform.position)
+        {
+            randomTarget = Random.insideUnitSphere * gameRadius * 0.9f;
+            decidedRandomPosition = true;
+        }
+        RandomMovementHelper();
+        if (waitTimeRandomMovementPassed >= 10.0f)
+        {
+            DecideAttackType();
+            decidedRandomPosition = false;
+            waitTimeRandomMovementPassed = 0.0f;
+        }
+    }
+
+    private void RandomMovementHelper()
+    {
+        FaceTarget(randomTarget);
+        float realSpeed = decideSpeed(randomTarget);
+        Vector3 direction = (randomTarget - transform.position).normalized;
+        enemyRigidBody.velocity = direction * realSpeed * Time.deltaTime;
+        Debug.DrawLine(transform.position, randomTarget, Color.green);
+    }
+
+    private void CirculateAroundTarget(Transform targetTransform)
+    {
+        timeCounter += Time.deltaTime;
+        float realSpeed = decideSpeed(targetTransform);
+        Vector3 direction = (targetTransform.position - transform.position).normalized;
+        enemyRigidBody.velocity = direction * realSpeed * Time.deltaTime + new Vector3(Mathf.Cos(timeCounter), Mathf.Sin(timeCounter), 0.0f) * radiusMovement;
+        if (timeCounter >= 360.0f) timeCounter = 0.0f;
     }
 
     private void MoveTowardsTarget(Transform targetTransform)
     {
+        float realSpeed = decideSpeed(targetTransform);
         Vector3 direction = (targetTransform.position - transform.position).normalized;
-        enemyRigidBody.velocity = direction * speed * Time.deltaTime;
+        enemyRigidBody.velocity = direction * realSpeed * Time.deltaTime;
+    }
+
+    private float decideSpeed(Transform targetTransform)
+    {
+        if (Vector3.Distance(targetTransform.position, transform.position) <= closeApproximity)
+            return speed / 5.0f;
+        return speed;
+    }
+
+    private float decideSpeed(Vector3 targetPosition)
+    {
+        if (Vector3.Distance(targetPosition, transform.position) <= closeApproximity)
+            return speed / 5.0f;
+        return speed;
     }
 
     private void FaceTarget(Transform targetTransform)
@@ -45,20 +137,31 @@ public class MoveAroundObject : MonoBehaviour
         enemyRigidBody.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5.0f);
     }
 
+    private void FaceTarget(Vector3 targetPosition)
+    {
+        Vector3 direction = (targetPosition - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        enemyRigidBody.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5.0f);
+    }
+
     private void Update()
     {
         if (playerIndexToAttack != -1) return;
         ChoosePlayerToAttack();
-        // decide how to attack ?
-        // random 50%: Smash into
-        // random 50%: rotate around
+        DecideAttackType();
+    }
+
+    private void DecideAttackType()
+    {
+        if (Random.value <= 0.2) attackType = 3;
+        else  attackType = Random.value <= 0.5 ? 1 : 2;
     }
 
     private void ChoosePlayerToAttack()
     {
         players = GameObject.FindGameObjectsWithTag(Utils.TagPlayer);
         if (players.Length <= 0) playerIndexToAttack = -1;
-        playerIndexToAttack = Random.Range(0, players.Length);
+        else playerIndexToAttack = Random.Range(0, players.Length);
     }
 
 
